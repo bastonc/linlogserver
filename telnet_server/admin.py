95594515d4d80af3dcb74bb0595592904e975ae0
Baston Sergey <bastonsv@gmail.com>
2020-08-07 13:16:52 +0300

    Admin service

--- telnet_server/admin.py
+++ telnet_server/admin.py
@@ -2,10 +2,16 @@ from django.contrib import admin
 from .models import Chek_update, Register_new, Version,  Template, Admins
 
 admin.site.register(Template)
-admin.site.register(Chek_update)
+#admin.site.register(Chek_update)
 admin.site.register(Register_new)
 admin.site.register(Version)
 admin.site.register(Admins)
+@admin.register(Chek_update)
+class Chek_updateAdmin(admin.ModelAdmin):
+    list_display = ('call', 'version', 'timestamp')
+    search_fields = ('call', 'version')
+    class Meta:
+        ordering = ("-timestamp")
 
 
 # Register your models here.
